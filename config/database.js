09b079a0d7c4c08ddb7067f514394e1bff53cbb9
sulivan22 <sulivan22@gmail.com>
2021-02-20 10:51:00 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     // 'url': 'mongodb://localhost/backendc2c'
-    'url': 'mongodb+srv://call2casa:Sulivandbl12h!@cluster0.8clfj.mongodb.net/call2casaBE?retryWrites=true&w=majority'
+    
 
 };
